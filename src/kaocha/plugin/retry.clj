8c6cce0fed592cb7f2165f937c1c322099f4a868
Andrea Crotti <andrea.crotti.0@gmail.com>
2020-12-03 14:45:33 +0000

    few more attempts

--- src/kaocha/plugin/retry.clj
+++ src/kaocha/plugin/retry.clj
@@ -1,7 +1,8 @@
 (ns kaocha.plugin.retry
   "Instrument/unstrument namespaces with Orchestra, to get validation of function
   arguments and return values based on clojure.spec.alpha."
-  (:require [kaocha.plugin :refer [defplugin]]
+  (:require [clojure.test :as te]
+            [kaocha.plugin :refer [defplugin]]
             [kaocha.hierarchy :as h]
             [kaocha.testable :as t]
             [kaocha.result :as r]))
@@ -10,32 +11,29 @@
 (def wait-time 100)
 ;; this needs to be reset in a fixture before every test
 (def current-retries (atom {}))
-
-(defn any-failed? []
-  (let [v (vals @current-retries)]
-    (and (seq v)
-         (= 3 (apply max v)))))
+(def report-history (atom []))
+(def to-report (atom nil))
 
 (defplugin kaocha.plugin/retry
-  (pre-report [event]
-    event
-    #_(if (any-failed?)
-        event
-        (assoc event :type :pass)))
-
-  #_(pre-run [test-plan]
-    (reset! current-retries {})
-    test-plan)
-
   (wrap-run [run test-plan]
     (fn [& args]
-      (let [result (apply run args)]
-        (if (kaocha.result/failed-one? result)
-          ;; retrying just once
-          (apply run args)
-          result))))
+      (let [result
+            (with-redefs [te/report (fn [e]
+                                      (swap! report-history conj e)
+                                      e)]
+              (apply run args))]
 
-  #_(post-test [test test-plan]
+        (doseq [{:keys [type] :as r} @report-history]
+          (if (= :pass type)
+            (te/report r)
+            (let [test-id (-> r :kaocha/testable :kaocha.testable/id)]
+              (println "test-id" test-id
+                       "current-retries" @current-retries)
+              (when (= max-retries (get @current-retries test-id))
+                (te/report r)))))
+        result)))
+
+  (post-test [test test-plan]
     (let [t-id (:kaocha.testable/id test)
           curr (get @current-retries t-id 0)]
       (when (and
@@ -44,15 +42,7 @@
              (< curr max-retries))
 
         (swap! current-retries assoc t-id (inc curr))
+        (println "Current retries" @current-retries)
         (Thread/sleep wait-time)
         (t/run-testable test test-plan))
-
-      ;;(tap> @current-retries)
       test)))
-
-(comment
-  (def debug-retries (atom nil))
-  (add-tap #(swap! debug-retries
-                   assoc
-                   (System/currentTimeMillis)
-                   %)))
