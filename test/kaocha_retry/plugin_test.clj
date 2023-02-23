3e866b233f74355621a0c659752b627a7cfceda6
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2023-02-23 17:39:08 +0100

    Support setting all options via config, too
    Config trumps defaults, CLI options trump config.

--- test/kaocha_retry/plugin_test.clj
+++ test/kaocha_retry/plugin_test.clj
@@ -1,5 +1,6 @@
 (ns kaocha-retry.plugin-test
   (:require [kaocha-retry.plugin :as sut]
+            [kaocha.plugin :as plugin]
             [clojure.test :refer [deftest testing is are]]))
 
 (deftest retry-test
@@ -16,3 +17,32 @@
 
         {:kaocha-retry.plugin/retrying-tests-regexes ["ns/flakey-*"], ::sut/retry? true}
         false))))
+
+(defn run-plugin-hook [hook init & extra-args]
+  (let [chain (plugin/load-all [:kaocha-retry.plugin/retry])]
+    (apply plugin/run-hook* chain hook init extra-args)))
+
+(deftest config-test
+  (is (= #::sut{:retry? true :max-retries 3 :retry-interval 100}
+         (run-plugin-hook :kaocha.hooks/config {})))
+
+  (is (= false (::sut/retry? (run-plugin-hook :kaocha.hooks/config
+                                              {::sut/retry? false}))))
+
+  (is (= true (::sut/retry? (run-plugin-hook :kaocha.hooks/config
+                                             {::sut/retry? false
+                                              :kaocha/cli-options {:retry true}}))))
+
+  (is (= 9 (::sut/max-retries (run-plugin-hook :kaocha.hooks/config
+                                               {::sut/max-retries 9}))))
+
+  (is (= 1 (::sut/max-retries (run-plugin-hook :kaocha.hooks/config
+                                               {::sut/max-retries 9
+                                                :kaocha/cli-options {:max-retries 1}}))))
+
+  (is (= 5000 (::sut/retry-interval (run-plugin-hook :kaocha.hooks/config
+                                                     {::sut/retry-interval 5000}))))
+
+  (is (= 200 (::sut/retry-interval (run-plugin-hook :kaocha.hooks/config
+                                                    {::sut/retry-interval 5000
+                                                     :kaocha/cli-options {:retry-interval 200}})))))
